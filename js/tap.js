369dc1c43233662f17e118aaa958f50559eadbd4
hyop <tlsgy0318@naver.com>
2022-05-22 20:38:15 +0900

    업로드

---
+++ js/tap.js
@@ -0,0 +1,6 @@
+$(function(){
+    $(".tab ul li").click(function(){
+        $(this).addClass('on').siblings().removeClass('on');
+        $("#"+$(this).data('id')).addClass('on').siblings().removeClass('on');
+    });
+});
