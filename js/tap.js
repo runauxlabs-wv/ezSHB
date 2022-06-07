1d0b038cf5697b6ce9d50bb5aa82e888878d1bc1
neeboynis <tlsgy0318@naver.com>
2022-06-07 17:26:37 +0900

    업로드

--- js/tap.js
+++ js/tap.js
@@ -3,4 +3,10 @@ $(function(){
         $(this).addClass('on').siblings().removeClass('on');
         $("#"+$(this).data('id')).addClass('on').siblings().removeClass('on');
     });
+
+    $(".indexBox ul li").click(function(){
+        var tabindex = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+        $(".indexBox .conBox").eq(tabindex).addClass('on').siblings().removeClass('on');
+    });
 });
