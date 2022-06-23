05fbda25da24c232b7400ae55fcd31792839a49f
neeboynis <tlsgy0318@naver.com>
2022-06-23 14:56:04 +0900

    업로드

--- js/newsroom.js
+++ js/newsroom.js
@@ -1,21 +1,20 @@
 var swiper = new Swiper(".mySwiper", {
-  breakpoints: { 
-    640: { 
+  breakpoints: {
+    320: {
       slidesPerView: 1,
+      spaceBetween: 7
     },
-    768: {
+    640: {
       slidesPerView: 2,
+      spaceBetween: 80
     },
-    1024: {
+    1500: {
       slidesPerView: 3,
-    },
+      spaceBetween: 40
+    }
   },
     autoplay : true,
     spaceBetween: 40,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev"
--- js/yk_story.js
+++ js/yk_story.js
@@ -1,6 +1,18 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 40,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1500: {
+      slidesPerView: 3,
+      spaceBetween: 50
+    }
+  },
     freeMode: true,
     pagination: {
       el: ".swiper-pagination",
