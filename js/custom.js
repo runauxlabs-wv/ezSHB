0acdfb841a71dd347e6042022f762245e5682186
neeboynis <tlsgy0318@naver.com>
2022-06-24 16:30:10 +0900

    업로드

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,9 @@ $(document).ready(function () {
     // var $moreBtn = $('header .top .h_inner .more_button');
     $('header .top .h_inner .more_button').click(function () {
         $(this).toggleClass('active');
+        $('header .top .h_inner .gnb').toggleClass('active');
+        $('header .top .h_inner .global').toggleClass('active');
+        $('header .top .h_inner .logo').toggleClass('active');
+        $('header .top .h_inner .more_logo').toggleClass('active');
     })
 });
