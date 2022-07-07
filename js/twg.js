6bde62cdd43f73632ed9a51546c91a9b90067a5b
neeboynis <tlsgy0318@naver.com>
2022-07-07 15:58:36 +0900

    업로드

--- js/twg.js
+++ js/twg.js
@@ -28,3 +28,34 @@ jQuery(document).ready(function(){
         //index넘버링으로 처리
     });
 });
+
+
+// 갤러리 미리보기
+$(document).ready(function() {
+    var xOffset = 10;
+    var yOffset = 30;
+
+    //마우스 오버시 preview 생성
+    $(document).on("mouseover",".gallery",function(e){
+        var image_data = $(this).data("image");
+        var add_caption = (image_data != undefined) ? "<br/>" + image_data : "" ;
+        $("body").append("<p id='preview'><img src='"+ $(this).attr("src") +"' width='300px' />"+ add_caption +"</p>");
+        $("#preview")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px")
+            .fadeIn("fast");
+    });
+    
+    //마우스 이동시 preview 이동
+    $(document).on("mousemove",".gallery",function(e){
+        $("#preview")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px");
+    });
+    
+    //마우스 아웃시 preview 제거
+    $(document).on("mouseout",".gallery",function(){
+        $("#preview").remove();
+    });
+  
+});
