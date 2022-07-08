0fc5f54a8fb33cd7bc0affd79b95f326d9987213
neeboynis <tlsgy0318@naver.com>
2022-07-08 16:19:57 +0900

    업로드

--- js/twg.js
+++ js/twg.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
     $(document).on("mouseover",".gallery",function(e){
         var image_data = $(this).data("image");
         var add_caption = (image_data != undefined) ? "<br/>" + image_data : "" ;
-        $("body").append("<p id='preview'><img src='"+ $(this).attr("src") +"' width='410px' />"+ add_caption +"</p>");
+        $("body").append("<p id='preview'><img src='"+ $(this).attr("src") +"' width='300px' />"+ add_caption +"</p>");
         $("#preview")
             .css("top",(e.pageY - xOffset) + "px")
             .css("left",(e.pageX + yOffset) + "px")
@@ -59,3 +59,71 @@ $(document).ready(function() {
     });
   
 });
+
+
+// 상품 이미지 슬라이드
+(function($) { "use strict";
+    
+  //Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+    t.style.top = n.clientY + "px", 
+    e.style.left = n.clientX + "px", 
+    e.style.top = n.clientY + "px", 
+    i.style.left = n.clientX + "px", 
+    i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+  
+  $(document).ready(function() {
+    
+    /* Hero Case study images */      
+    
+    $('.case-study-name:nth-child(1)').on('mouseenter', function() {
+      $('.case-study-name.active').removeClass('active');
+      $('.case-study-images li.show').removeClass("show");
+      $('.case-study-images li:nth-child(1)').addClass("show");
+      $('.case-study-name:nth-child(1)').addClass('active');
+    })
+    $('.case-study-name:nth-child(2)').on('mouseenter', function() {
+      $('.case-study-name.active').removeClass('active');
+      $('.case-study-images li.show').removeClass("show");
+      $('.case-study-images li:nth-child(2)').addClass("show");
+      $('.case-study-name:nth-child(2)').addClass('active');
+    })
+    $('.case-study-name:nth-child(3)').on('mouseenter', function() {
+      $('.case-study-name.active').removeClass('active');
+      $('.case-study-images li.show').removeClass("show");
+      $('.case-study-images li:nth-child(3)').addClass("show");
+      $('.case-study-name:nth-child(3)').addClass('active');
+    })
+    $('.case-study-name:nth-child(4)').on('mouseenter', function() {
+      $('.case-study-name.active').removeClass('active');
+      $('.case-study-images li.show').removeClass("show");
+      $('.case-study-images li:nth-child(4)').addClass("show");
+      $('.case-study-name:nth-child(4)').addClass('active');
+    })
+    $('.case-study-name:nth-child(1)').trigger('mouseenter')
+          
+  });
+
+  
+  
+  
+  })(jQuery); 
