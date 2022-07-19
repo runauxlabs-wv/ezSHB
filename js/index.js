9c7f38d16379e0d51236dd86e3c63962bbca5a40
neeboynis <tlsgy0318@naver.com>
2022-07-19 11:18:51 +0900

    업로드

--- js/index.js
+++ js/index.js
@@ -40,4 +40,24 @@ $(function() {
 
 
 
+//  테스트중
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+var modal = document.getElementsByClassName("modal")[0];
+// modal.addEventListener("click", function(e) {
+//   modal.classList.remove("active");
+// });
+
+var info = document.getElementsByClassName("about")[0];
+info.addEventListener("click", function() {
+  modal.classList.add("active");
+});
+
+var close = document.getElementsByClassName("modal__card__close")[0];
+close.addEventListener("click", function() {
+  modal.classList.remove("active");
+});
+
+
  
